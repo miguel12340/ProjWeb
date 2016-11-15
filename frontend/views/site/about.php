f2c2e63f45b90ba9c7b14179d403d5e7152711c6
miguel12340 <miguelrochacalhau@hotmail.com>
2016-11-15 15:49:59 +0000

    Created
    Created

---
+++ frontend/views/site/about.php
@@ -0,0 +1,28 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+use frontend\assets\AppAsset;
+
+$this->title = 'Sobre Nós';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="container-fluid">
+
+    	<div class="back_for_text">
+		    <p><b>QWARTUS</b> é o website que lhe permite encontrar o seu quarto ao melhor preço.</p>
+
+		    <p>Também irá disponibilizar suporte a quem permita colocar quartos a arrendar de maneira a que encontre o arrendatário ao seu gosto.</p>
+			
+			<p>Com este serviço disponivel a nivel web e android, o QWARTUS é utilizado diariamente por centenas de universitários que não conseguem encontrar o seu quarto de sonho.</p>
+			
+			<p>O QWARTUS foi desenvolvido num âmbito de um projecto, que se transformou muito mais que isso.</p>    		
+    	</div>
+
+    </div>	
+
+</div>
---
+++ frontend/views/site/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+
+$this->title = 'Qwartus - Home';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Qwartus</h1>
+        <p>Onde os quartos estão á distância de um click!</p>
+
+    </div>
+
+    <div class="body-content" align="center">
+
+        <div class="row">
+            <div class="col-md-6">
+                <p>
+                <?= 
+                    Html::a('Regista-te', ['/site/signup'], ['class' => 'btn btn-primary btn-lg']) 
+                 ?>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p>
+                <?= 
+                    Html::a('Pesquisa', ['/site/index'], ['class' => 'btn btn-primary btn-lg']) 
+                 ?>
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
